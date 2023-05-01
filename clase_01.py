dd8800177124fe87a1462f7827b8354316e98324
Cristian Abel <cristtian.abel@gmail.com>
2023-05-01 14:21:08 -0400

    create clase_01.py

---
+++ clase_01.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+
+pygame.init()
+
+#creando una ventana
+import pygame
+import sys
+
+pygame.init()
+size = (800,600)
+screen = pygame.display.set_mode(size)
+while True:
+    for event in pygame.event.get():
+        print(event) # vemos todos los eventos que ocurren
+        if event.type == pygame.QUIT:
+            pygame.quit() #salimos de la ventana
+            sys.exit()
+
